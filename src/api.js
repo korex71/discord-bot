f684b07caca9a312ecbcf7fe82da1a88d929e02d
Gabriel A <kore.dv7@gmail.com>
2021-06-28 13:41:06 -0300

    Fix headers authorization

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 const axios = require("axios").default;
 
+axios.defaults.headers.common["x-api-key"] = process.env.CAT_API_KEY;
+
 const api = axios.create({
   baseURL: "https://api.thecatapi.com/",
 });
-
-api.default.headers.common["x-api-key"] = process.env.CAT_API_KEY;
 
 module.exports = api;
