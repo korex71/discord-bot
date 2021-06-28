c9264919bf01b2a469c84cfe36c2c8adfe6bc770
Gabriel A <kore.dv7@gmail.com>
2021-06-28 13:50:54 -0300

    Update webpage

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const express = require("express");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.listen(PORT, () => console.log(PORT));
 
 // Distube Class
